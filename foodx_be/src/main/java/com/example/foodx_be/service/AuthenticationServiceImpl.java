b7daf2f1b69e6cac346aa8d154cc649f8ba388d2
DwngLee <156188368+DwngLee@users.noreply.github.com>
2024-06-02 21:58:37 +0700

    update: disable cros

--- foodx_be/src/main/java/com/example/foodx_be/service/AuthenticationServiceImpl.java
+++ foodx_be/src/main/java/com/example/foodx_be/service/AuthenticationServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.foodx_be.service;
 
 import com.example.foodx_be.dto.request.AuthenticationRequest;
 import com.example.foodx_be.dto.response.AuthenticationResponse;
+import com.example.foodx_be.enity.User;
 import com.example.foodx_be.exception.UnAuthenticatedException;
 import com.example.foodx_be.exception.UserNotFoundException;
 import com.example.foodx_be.repository.UserRepository;
@@ -12,14 +13,16 @@ import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.StringJoiner;
 
 @Service
 @AllArgsConstructor
-public class AuthenticationImpl implements AuthenticationService {
+public class AuthenticationServiceImpl implements AuthenticationService {
     private UserRepository userRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -31,22 +34,23 @@ public class AuthenticationImpl implements AuthenticationService {
         if (!authenticated) {
             throw new UnAuthenticatedException();
         }
-        var token = generateToken(authenticationRequest.getUsername());
+        var token = generateToken(user);
         return AuthenticationResponse.builder()
                 .token(token)
                 .authenticated(true)
                 .build();
     }
 
-    private String generateToken(String username) {
+    private String generateToken(User user) {
         JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS512);
         //data trong body dc goi la claim
         //claim tieu chuan:
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-                .subject(username)
+                .subject(user.getUsername())
                 .issuer("DuongLe")//issue tu ai, thuong la domain service
                 .issueTime(new Date())
                 .expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()))
+                .claim("scope", buildScope(user))
                 .build();
 
         Payload payload = new Payload(jwtClaimsSet.toJSONObject());
@@ -60,5 +64,13 @@ public class AuthenticationImpl implements AuthenticationService {
         } catch (JOSEException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String buildScope(User user) {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if (!CollectionUtils.isEmpty(user.getRoles())) {
+            user.getRoles().forEach(stringJoiner::add);
+        }
+        return stringJoiner.toString();
     }
 }
